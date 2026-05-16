021adf031d8be945a537b020f14c77fb64e8b012
전동희 <bethetopone@gmail.com>
2026-05-16 18:09:31 +0900

    필터 버튼 active 처리

--- src/components/AnimalList.tsx
+++ src/components/AnimalList.tsx
@@ -4,20 +4,25 @@ import AnimalCard from "./AnimalCard";
 
 function AnimalList() {
   const [animals, setAnimals] = useState(animalData);
+  const [selectedStatus, setselectedStatus] = useState("ALL");
+
   const handleShowAll = () => {
     setAnimals(animalData);
+    setselectedStatus("ALL");
   };
   const handleShowActive = () => {
     const activeAnimals = animalData.filter(
       (animal) => animal.status === "ACTIVE",
     );
     setAnimals(activeAnimals);
+    setselectedStatus("ACTIVE");
   };
   const handleShowResting = () => {
     const restingAnimals = animalData.filter(
       (animal) => animal.status === "RESTING",
     );
     setAnimals(restingAnimals);
+    setselectedStatus("RESTING");
   };
 
   return (
@@ -25,13 +30,25 @@ function AnimalList() {
       <h2>동물 목록</h2>
       <p>현재 동물 수 : ({animals.length})</p>
       <div className="filter-buttons">
-        <button type="button" onClick={handleShowAll}>
+        <button
+          type="button"
+          className={selectedStatus === "ALL" ? "active" : ""}
+          onClick={handleShowAll}
+        >
           전체
         </button>
-        <button type="button" onClick={handleShowActive}>
+        <button
+          type="button"
+          className={selectedStatus === "ACTIVE" ? "active" : ""}
+          onClick={handleShowActive}
+        >
           관리중
         </button>
-        <button type="button" onClick={handleShowResting}>
+        <button
+          type="button"
+          className={selectedStatus === "RESTING" ? "active" : ""}
+          onClick={handleShowResting}
+        >
           휴식중
         </button>
       </div>
