ed87eeef7b3a00e66cf2ce6d16af4511ece39ff3
전동희 <bethetopone@gmail.com>
2026-05-16 17:44:57 +0900

    Dummy data 를 setState 로 받아서 처리, css 추가 있음

--- src/components/AnimalList.tsx
+++ src/components/AnimalList.tsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import { animalData } from "../data/animalData";
 import AnimalCard from "./AnimalCard";
 
 function AnimalList() {
+  const [animals, setAnimals] = useState(animalData);
+  const handleShowAll = () => {
+    setAnimals(animalData);
+  };
+  const handleShowActive = () => {
+    const activeAnimals = animalData.filter(
+      (animal) => animal.status === "ACTIVE",
+    );
+    setAnimals(activeAnimals);
+  };
+  const handleShowResting = () => {
+    const restingAnimals = animalData.filter(
+      (animal) => animal.status === "RESTING",
+    );
+    setAnimals(restingAnimals);
+  };
+
   return (
     <section className="animal-list">
       <h2>동물 목록</h2>
+      <p>현재 동물 수 : ({animals.length})</p>
+      <div className="filter-buttons">
+        <button type="button" onClick={handleShowAll}>
+          전체
+        </button>
+        <button type="button" onClick={handleShowActive}>
+          관리중
+        </button>
+        <button type="button" onClick={handleShowResting}>
+          휴식중
+        </button>
+      </div>
       <ul>
-        {animalData.map((animal) => (
+        {animals.map((animal) => (
           <AnimalCard key={animal.id} animal={animal} />
         ))}
       </ul>
