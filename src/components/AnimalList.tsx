9aabbcd36b4ce7dce7b4f9b0d9c663b531d06788
전동희 <bethetopone@gmail.com>
2026-05-17 19:09:01 +0900

    error 상태 추가, try catch 처리, 에러 화면 테스트, renderAnimalList 로 조건부 렌더링 처리

--- src/components/AnimalList.tsx
+++ src/components/AnimalList.tsx
@@ -10,13 +10,23 @@ function AnimalList() {
   const [animals, setAnimals] = useState<Animal[]>([]); //animals는 Animal 객체들이 들어가는 배열
   const [selectedStatus, setSelectedStatus] = useState<FilterStatus>("ALL");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchAnimals = async () => {
-      setLoading(true);
-      const data = await getAnimals(); //getAnimals() 결과가 올 때까지 기다린다. 결과가 오면 data에 담는다.
-      setAnimals(data);
-      setLoading(false);
+      try {
+        setLoading(true);
+        setError(""); // 이전 에러 초기화
+
+        const data = await getAnimals(); //getAnimals() 결과가 올 때까지 기다린다. 결과가 오면 data에 담는다.
+
+        setAnimals(data);
+      } catch {
+        setError("동물 데이터를 불러오지 못했습니다.");
+      } finally {
+        // 성공하든 실패하든 마지막에 무조건 실행.
+        setLoading(false);
+      }
     };
     fetchAnimals();
   }, []);
@@ -39,6 +49,30 @@ function AnimalList() {
     setAnimals(filteredAnimals);
     setSelectedStatus(status);
     setLoading(false);
+  };
+
+  // 동물 리스트 카드 (early return 방식 : 예외 상황 먼저 처리 → 마지막에 정상 화면 return)
+  // 데이터 loading, error, empty, success 처리
+  const renderAnimalList = () => {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (error) {
+      return <p className="error-message">{error}</p>;
+    }
+
+    if (animals.length === 0) {
+      return <p className="empty-message">조회된 동물이 없습니다.</p>;
+    }
+
+    return (
+      <ul>
+        {animals.map((animal) => (
+          <AnimalCard key={animal.id} animal={animal} />
+        ))}
+      </ul>
+    );
   };
 
   return (
@@ -75,17 +109,8 @@ function AnimalList() {
           이동완료
         </button>
       </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : animals.length === 0 ? (
-        <p className="empty-message">조회된 동물이 없습니다.</p>
-      ) : (
-        <ul>
-          {animals.map((animal) => (
-            <AnimalCard key={animal.id} animal={animal} />
-          ))}
-        </ul>
-      )}
+
+      {renderAnimalList()}
     </section>
   );
 }
--- src/services/animalService.ts
+++ src/services/animalService.ts
@@ -4,9 +4,10 @@ import type { Animal } from "../types/animal";
 // (): Promise<Animal[]> 이 함수는 나중에 Animal 배열을 반환하는 Promise다.
 export const getAnimals = (): Promise<Animal[]> => {
   // 지금 바로 데이터를 주지 않고, 나중에 데이터를 줄게
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     // 0.5초 뒤에 animalData를 반환 완료 처리
     setTimeout(() => {
+      // reject(new Error("API Error Test")); // 에러 테스트
       resolve(animalData);
     }, 500);
   });
