4c06536bb33a541875d3d4df699e8c39960d7d11
전동희 <bethetopone@gmail.com>
2026-05-17 16:57:52 +0900

    동기 데이터를 비동기 데이터처럼 바꾸기

--- src/services/animalService.ts
+++ src/services/animalService.ts
@@ -1,5 +1,11 @@
 import { animalData } from "../data/animalData";
 
 export const getAnimals = () => {
-  return animalData;
+  // 지금 바로 데이터를 주지 않고, 나중에 데이터를 줄게
+  return new Promise((resolve) => {
+    // 0.5초 뒤에 animalData를 반환 완료 처리
+    setTimeout(() => {
+      resolve(animalData);
+    }, 500);
+  });
 };
