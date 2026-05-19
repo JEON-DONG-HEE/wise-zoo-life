c220a26057054769bcdcea6f077f414501563f18
전동희 <bethetopone@gmail.com>
2026-05-19 16:47:17 +0900

    리스트 페이지에 등록 버튼 추가 및 페이지 이동

--- src/pages/AnimalListPage.tsx
+++ src/pages/AnimalListPage.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import CommonButton from "../components/CommonButton";
 import AnimalList from "../components/AnimalList";
 
 function AnimalListPage() {
+  const navigate = useNavigate();
+
   return (
     <main className="page-content">
       <section className="summary-card">
         <h2>동물 정보 관리</h2>
         <p>동물 목록, 상태, 담당 사육사 정보를 관리합니다.</p>
+
+        <CommonButton
+          variant="primary"
+          onClick={() => navigate("/animals/new")}
+        >
+          동물 등록
+        </CommonButton>
       </section>
 
       <AnimalList />
