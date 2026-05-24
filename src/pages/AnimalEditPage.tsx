993dcfc05914696e8543edc49cd1e83e67cf8d2a
전동희 <bethetopone@gmail.com>
2026-05-24 15:48:45 +0900

    수정 폼 입력값 변경 가능을 콘솔로 확인

--- src/pages/AnimalEditPage.tsx
+++ src/pages/AnimalEditPage.tsx
@@ -1,7 +1,13 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState, type ChangeEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type SyntheticEvent,
+} from "react";
 import { getAnimalById } from "../services/animalService";
 import type { Animal, AnimalStatus } from "../types/animal";
+import CommonButton from "../components/CommonButton";
 
 type AnimalFormValues = {
   name: string;
@@ -16,6 +22,7 @@ type AnimalFormValues = {
 function AnimalEditPage() {
   const { id } = useParams();
   const animalId = Number(id);
+  const navigate = useNavigate();
   const [animal, setAnimal] = useState<Animal | null>(null);
   const [formValues, setFormValues] = useState<AnimalFormValues>({
     name: "",
@@ -64,86 +71,103 @@ function AnimalEditPage() {
     });
   };
 
+  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("수정할 폼 데이터 : ", formValues);
+  };
+
   return (
     <main className="page-content">
       <section className="summary-card">
         <h2>동물 정보 수정</h2>
         <p>기존 동물 정보를 수정합니다.</p>
 
-        <div className="form-field">
-          <label htmlFor="name">동물이름</label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            value={formValues.name}
-            onChange={handleChange}
-            placeholder="예: 레오"
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="species">종</label>
-          <input
-            id="species"
-            name="species"
-            type="text"
-            value={formValues.species}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="department">부서</label>
-          <input
-            id="department"
-            name="department"
-            type="text"
-            value={formValues.department}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="keeper">담당 사육사</label>
-          <input
-            id="keeper"
-            name="keeper"
-            type="text"
-            value={formValues.keeper}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="age">나이</label>
-          <input
-            id="age"
-            name="age"
-            type="text"
-            value={formValues.age}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="status">상태</label>
-          <select
-            id="status"
-            name="status"
-            value={formValues.status}
-            onChange={handleChange}
-          >
-            <option value="ACTIVE">관리중</option>
-            <option value="RESTING">휴식중</option>
-            <option value="TRANSFERRED">이동완료</option>
-          </select>
-        </div>
-        <div className="form-field">
-          <label htmlFor="joinedDate">등록일</label>
-          <input
-            id="joinedDate"
-            name="joinedDate"
-            type="date"
-            value={formValues.joinedDate}
-            onChange={handleChange}
-          />
-        </div>
+        <form className="animal-form" onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label htmlFor="name">동물이름</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              value={formValues.name}
+              onChange={handleChange}
+              placeholder="예: 레오"
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="species">종</label>
+            <input
+              id="species"
+              name="species"
+              type="text"
+              value={formValues.species}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="department">부서</label>
+            <input
+              id="department"
+              name="department"
+              type="text"
+              value={formValues.department}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="keeper">담당 사육사</label>
+            <input
+              id="keeper"
+              name="keeper"
+              type="text"
+              value={formValues.keeper}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="age">나이</label>
+            <input
+              id="age"
+              name="age"
+              type="number"
+              value={formValues.age}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="status">상태</label>
+            <select
+              id="status"
+              name="status"
+              value={formValues.status}
+              onChange={handleChange}
+            >
+              <option value="ACTIVE">관리중</option>
+              <option value="RESTING">휴식중</option>
+              <option value="TRANSFERRED">이동완료</option>
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="joinedDate">등록일</label>
+            <input
+              id="joinedDate"
+              name="joinedDate"
+              type="date"
+              value={formValues.joinedDate}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-actions">
+            <CommonButton variant="primary" type="submit">
+              저장
+            </CommonButton>
+            <CommonButton variant="secondary" onClick={() => navigate(-1)}>
+              취소
+            </CommonButton>
+          </div>
+        </form>
       </section>
     </main>
   );
