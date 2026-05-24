4904b8d712b552ab00214da5b649d28c569b2761
전동희 <bethetopone@gmail.com>
2026-05-24 11:06:18 +0900

    상세 -> 수정 페이지 연결

--- src/pages/AnimalDetailPage.tsx
+++ src/pages/AnimalDetailPage.tsx
@@ -86,6 +86,12 @@ function AnimalDetailPage() {
           </CommonButton>
           <CommonButton
             variant="danger"
+            onClick={() => navigate(`/animals/${animalId}/edit`)}
+          >
+            수정
+          </CommonButton>
+          <CommonButton
+            variant="danger"
             onClick={() => setIsDeleteModalOpen(true)}
           >
             삭제
